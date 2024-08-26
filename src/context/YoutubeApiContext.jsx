ed72a09b02679789ffbbc182b81bb8ba777c1563
twtwkim <xodhks14843@naver.com>
2024-08-26 13:43:07 +0900

    수정1

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,8 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
-import FakeYoutubeClient from "../api/fakeYoutubeClient.js";
 import YoutubeClient from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
-// const client = new FakeYoutubeClient();
 const client = new YoutubeClient();
 
 const youtube = new Youtube(client);
